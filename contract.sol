1888c48e2481e787794b4ecace7a80408c220463
bchip <bachippi@svsu.edu>
2016-06-21 17:08:21 -0400

    0.0.1

--- contract.sol
+++ contract.sol
@@ -1,13 +1,9 @@
 contract gamblingEvent {
-    // Parameters of the auction. Times are either
-    // absolute unix timestamps (seconds since 1970-01-01)
-    // or time periods in seconds.
+
     address public contractOwner;
     uint public numBets;
     uint public maxBet;
-    // Current state of the auction.
-    //address public highestBidder;
-    //uint public highestBid;
+
     address[] addresses;
     uint[] numbers;
     uint oldNumber = 0;
@@ -16,76 +12,35 @@ contract gamblingEvent {
 
     struct Gambler{
     	bool gambled;
+    };
 
-    }
-;
-    // Allowed withdrawals of previous bids
-    //mapping(address => uint) pendingReturns;
     mapping(address => Gambler) gamblers;
 
-    // Set to true at the end, disallows any change
     bool ended;
 
-    // Events that will be fired on changes.
-    //event HighestBidIncreased(address bidder, uint amount);
-    //event AuctionEnded(address winner, uint amount);
     event betSubmit(address gambler, uint amount);
-    event gamblingEnded(address winner, uint amount);
+    event gamblingEnd(address winner, uint amount);
 
-    // The following is a so-called natspec comment,
-    // recognizable by the three slashes.
-    // It will be shown when the user is asked to
-    // confirm a transaction.
-
-    /// Create a simple auction with `_biddingTime`
-    /// seconds bidding time on behalf of the
-    /// beneficiary address `_beneficiary`.
-    /* function SimpleAuction(uint _biddingTime, address _beneficiary) {
-        beneficiary = _beneficiary;
-        auctionStart = now;
-        biddingTime = _biddingTime;
-    } */
     function gamblingEvent(_maxBet){
         contractOwner = msg.sender;
         numBets = 0;
         maxBet = _maxBet;
     }
 
-    /// Bid on the auction with the value sent
-    /// together with this transaction.
-    /// The value will only be refunded if the
-    /// auction is not won.
     function bid() {
     	Gambler sender = gamblers[msg.sender];
         if (sender.gambled) throw;
-        // No arguments are necessary, all
-        // information is already part of
-        // the transaction.
-        if (numBets > 10) {
-            // Revert the call if the bidding
-            // period is over.
-            throw;
-        }
-        /* if (msg.value <= highestBid) {
-            // If the bid is not higher, send the
-            // money back.
-            throw;
-        } */
-        /* if (highestBidder != 0) {
-            // Sending back the money by simply using
-            // highestBidder.send(highestBid) is a security risk
-            // because it can be prevented by the caller by e.g.
-            // raising the call stack to 1023. It is always safer
-            // to let the recipient withdraw their money themselves.
-            pendingReturns[highestBidder] += highestBid;
-        } */
-        if (msg.value != maxBet){
-            throw;
-        }
-        /* highestBidder = msg.sender;
-        highestBid = msg.value; */
+
+        if (ended) throw;
+     
+        if (msg.value != maxBet) throw;
+
         sender.gambled = true;
-        addresses.push((now * maxBet) / (block.number / addresses.length) 
+        addresses.push(msg.sender);
+
+        numbers.push(uint8(sha3(mulmod((now+uint(tx.origin)), (block.number+msg.value+tx.gasprice), 
+        	(uint(msg.sender)+block.number+block.difficulty))));) 
+
         betSubmit(msg.sender, msg.value);
         numBets = numBets + 1;
         if(numBets == 10){
@@ -99,41 +54,24 @@ contract gamblingEvent {
 	        houseAmount = this.balance / 2000;
 	        owner.send(houseAmount);
 	        winner.send(this.balance);
+	        gamblingEnded();
         }
     }
 
-    /// Withdraw a bid that was overbid.
-    /* function withdraw() {
-        var amount = pendingReturns[msg.sender];
-        // It is important to set this to zero because the recipient
-        // can call this function again as part of the receiving call
-        // before `send` returns.
-        pendingReturns[msg.sender] = 0;
-        if (!msg.sender.send(amount))
-            throw; // If anything fails, this will revert the changes above
-    } */
 
-    /// End the auction and send the highest bid
-    /// to the beneficiary.
-    function auctionEnd() {
-        if (numBets <= 10)
-            throw; // auction did not yet end
+    function gamblingEnded() {
+        if (numBets < 10)
+            throw;
         if (ended)
-            throw; // this function has already been called
-        gamblingEnded(highestBidder, highestBid);
+            throw; 
+        gamblingEnd(highestBidder, highestBid);
 
         ended = true;
     }
 
     function () {
-        // This function gets executed if a
-        // transaction with invalid data is sent to
-        // the contract or just ether without data.
-        // We revert the send so that no-one
-        // accidentally loses money when using the
-        // contract.
         throw;
     }
 }
 
-return uint8(sha3(mulmod((now+uint(tx.origin)), (block.number+msg.value+tx.gasprice), (uint(msg.sender)+block.number+block.difficulty))));
+
